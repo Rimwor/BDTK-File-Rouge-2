175ac9473aacd4a6fbf9918d4bada320ceeffde7
Beata Krzyżosiak <80414308+Rimwor@users.noreply.github.com>
2021-10-19 11:18:28 +0200

    >> footer visual adjustment - Beata

--- vues/index_3footer.php
+++ vues/index_3footer.php
@@ -9,9 +9,18 @@
         <div class="col">
             <!-- empty -->
         </div>
+        <?php if ($action != 'admin') { ?> 
         <div class="col mr-1 d-flex align-items-end flex-column">
             <a href="index.php?action=admin"><button id="btnback" name="back" class="btn btn-light btn-sm mb-1 mt-1">Retour</button></a>
         </div>
+        <?php } ?>
+
+        <?php if ($action == 'admin') { ?>
+        <div class="col mr-1 d-flex align-items-end flex-column">
+            <br/>
+        </div>
+        <?php } ?>
+        
     </div>
     </div>
 </footer>
